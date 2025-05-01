1f82551856462eab75e1402ac6182a9602cb0a5b
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-05-01 02:44:54 -0500

    feat: add golang formatter

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -23,6 +23,7 @@ return {
     require("conform").setup({
       formatters_by_ft = {
         lua = { "stylua" },
+        go = { "gofumpt", "goimports" },
         nix = { "nixfmt-rfc-style" },
       },
       format_on_save = {
