998b371943c31016a0fc2d6a709d3d60126761e2
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-05-01 00:31:45 -0500

    fix(conform): properly create keymap

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -4,12 +4,24 @@
 
 return {
   "conform.nvim",
-  event = "DeferredUIEnter",
+  keys = {
+    { "<leader>FF", function()end, desc = "[F]ormat [F]ile" },
+  },
   after = function(_)
-    require("conform").setup({
+    local conform = require("conform")
+
+    conform.setup({
       formatters_by_ft = {
         lua = { "stylua" },
       },
     })
+
+    vim.keymap.set({ "n", "v" }, "<leader>FF", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "[F]ormat [F]ile" })
   end,
 }
