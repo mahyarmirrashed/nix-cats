9fa3c111a447e4023203dbca7d61de0fa9ca6f24
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-05-01 00:58:11 -0500

    fix(conform): trigger for formatting on save

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -3,25 +3,31 @@
 -------------------------------------------------------------------------------
 
 return {
-  "conform.nvim",
-  keys = {
-    { "<leader>FF", function()end, desc = "[F]ormat [F]ile" },
-  },
-  after = function(_)
-    local conform = require("conform")
+	"conform.nvim",
+	event = { "BufWritePre" },
+	keys = {
+		{ "<leader>FF", function() end, desc = "[F]ormat [F]ile" },
+	},
+	after = function(_)
+		local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-      },
-    })
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				nix = { "nixfmt-rfc-style" },
+			},
+			format_on_save = {
+				lsp_format = "fallback",
+				timeout_ms = 500,
+			},
+		})
 
-    vim.keymap.set({ "n", "v" }, "<leader>FF", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      })
-    end, { desc = "[F]ormat [F]ile" })
-  end,
+		vim.keymap.set({ "n", "v" }, "<leader>FF", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "[F]ormat [F]ile" })
+	end,
 }
