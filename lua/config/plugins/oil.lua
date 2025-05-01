da531487987407fc6c3f9a30b7c55a49d1abcfe8
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-05-01 14:13:03 -0500

    refactor(oil): simplify to just unnested table

--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -3,33 +3,31 @@
 -------------------------------------------------------------------------------
 
 return {
-  {
-    "oil.nvim",
-    dependencies = { "mini.icons" },
-    lazy = false,
-    keys = {
-      { "<leader>e", "<cmd>Oil<CR>", desc = "Toggle Oil file explorer" },
-    },
-    after = function(_)
-      require("oil").setup({
-        -- Replace `netrw` as default file explorer
-        default_file_explorer = true,
-        -- Keymaps in oil buffer
-        keymaps = {
-          ["h"] = { "actions.parent", mode = "n" },
-          ["l"] = { "actions.select", mode = "n" },
-        },
-        -- Columns to show for files
-        columns = {
-          "icon",
-          "permissions",
-          "size",
-        },
-        view_options = {
-          -- Show files and directories that start with "."
-          show_hidden = true,
-        },
-      })
-    end,
+  "oil.nvim",
+  dependencies = { "mini.icons" },
+  lazy = false,
+  keys = {
+    { "<leader>e", "<cmd>Oil<CR>", desc = "Toggle Oil file explorer" },
   },
+  after = function(_)
+    require("oil").setup({
+      -- Replace `netrw` as default file explorer
+      default_file_explorer = true,
+      -- Keymaps in oil buffer
+      keymaps = {
+        ["h"] = { "actions.parent", mode = "n" },
+        ["l"] = { "actions.select", mode = "n" },
+      },
+      -- Columns to show for files
+      columns = {
+        "icon",
+        "permissions",
+        "size",
+      },
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+      },
+    })
+  end,
 }
