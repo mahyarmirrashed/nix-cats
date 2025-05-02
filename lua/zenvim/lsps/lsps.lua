15eb48a5e9174bb947b974cbcdc92c4575ae217a
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-05-02 00:33:57 -0500

    refactor(lsp): use lze to set up keymaps

--- lua/zenvim/lsps/lsps.lua
+++ lua/zenvim/lsps/lsps.lua
@@ -1,29 +1,24 @@
-local function on_attach(_, bufnr)
-  -- Helper function for normal-mode keymaps
-  local nmap = function(keys, func, desc)
-    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
-  end
-
-  -- Essential LSP keybindings
-  nmap("<leader>lr", vim.lsp.buf.rename, "Rename")
-  nmap("<leader>la", vim.lsp.buf.code_action, "Code Action")
-  nmap("<leader>lh", vim.lsp.buf.signature_help, "Signature Documentation")
-  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-  nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-  nmap("gd", vim.lsp.buf.definition, "Goto Definition")
-  nmap("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
-
-  vim.api.nvim_buf_create_user_command(
-    bufnr,
-    "Format",
-    function(_) vim.lsp.buf.format() end,
-    { desc = "Format current buffer with LSP" }
-  )
-end
-
--- Define LSP servers
-local servers = {
+require("lze").load({
+  {
+    "nvim-lspconfig",
+    on_require = { "lspconfig" },
+    keys = {
+      { "<leader>rn", vim.lsp.buf.rename, "Rename" },
+      { "<leader>ca", vim.lsp.buf.code_action, "Code Action" },
+      { "K", vim.lsp.buf.hover, "Hover Documentation" },
+      { "<c-k>", vim.lsp.buf.signature_help, "Signature Documentation" },
+    },
+    lsp = function(plugin)
+      vim.lsp.config(plugin.name, plugin.lsp or {})
+      vim.lsp.enable(plugin.name)
+    end,
+    before = function()
+      vim.lsp.config("*", {
+        capabilities = vim.lsp.protocol.make_client_capabilities(),
+      })
+    end,
+  },
+  -- LSP server definitions
   {
     "lua_ls",
     lsp = {
@@ -48,24 +43,5 @@ local servers = {
     lsp = {
       filetypes = { "go", "gomod", "gowork", "gotmpl" },
     },
-  }
-}
-
--- Load LSP servers with client capabilities
-require("lze").load({
-  {
-    "nvim-lspconfig",
-    on_require = { "lspconfig" },
-    lsp = function(plugin)
-      vim.lsp.config(plugin.name, plugin.lsp or {})
-      vim.lsp.enable(plugin.name)
-    end,
-    before = function()
-      vim.lsp.config("*", {
-        on_attach = on_attach,
-        capabilities = vim.lsp.protocol.make_client_capabilities(),
-      })
-    end,
   },
-  servers,
 })
