cc8c2a6560aadfc44af4d2385044b8c30f308836
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-05-02 00:50:58 -0500

    feat(keymaps): add keymap to close buffer

--- lua/zenvim/keymaps.lua
+++ lua/zenvim/keymaps.lua
@@ -1,4 +1,5 @@
-vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>bd", ":bd<cr>", { desc = "Close Buffer", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save File" })
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
-vim.keymap.set("i", "jj", "<esc>", { desc = "Exit insert mode" })
-vim.keymap.set("i", "jk", "<esc>", { desc = "Exit insert mode" })
+vim.keymap.set("i", "jj", "<esc>", { desc = "Exit Insert Mode" })
+vim.keymap.set("i", "jk", "<esc>", { desc = "Exit Insert Mode" })
