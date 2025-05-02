775e09097e2100f92894114572deec3c8eb8e334
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-05-01 19:06:35 -0500

    fix: ensure that cache directory is made

--- lua/zenvim/init.lua
+++ lua/zenvim/init.lua
@@ -4,6 +4,9 @@ vim.g.mapleader = " "
 -- Enables setting LSPs within the `lze` specs
 require("lze").register_handlers(require("lzextras").lsp)
 
+-- Ensure that cache directory is made
+vim.fn.mkdir(vim.fn.stdpath("cache"), "p")
+
 -- Source configuration modules
 require("zenvim.autocmds")
 require("zenvim.keymaps")
